320bc81924785b7981af08c0f33714e473df1ca1
Conlan Cesar <conlan.cesar@datadoghq.com>
2026-06-05 10:53:40 -0400

    Add Linuxbrew support

--- Casks/pants.rb
+++ Casks/pants.rb
@@ -1,15 +1,31 @@
 cask "pants" do
-  version "0.13.2"
-  sha256 "a6f3231413ca1f793caffa621171a4b1a0158e7488cd0b5bb3e742cb99cc72a8"
+  arch arm: "aarch64", intel: "x86_64"
+  os macos: "macos-aarch64", linux: "linux-#{arch}"
 
-  url "https://github.com/pantsbuild/scie-pants/releases/download/v#{version}/scie-pants-macos-aarch64"
+  artifact = "scie-pants-#{os}"
+
+  sha256 arm:          "a6f3231413ca1f793caffa621171a4b1a0158e7488cd0b5bb3e742cb99cc72a8",
+         arm64_linux:  "b40b60e50e9cb69e13029e100be995fbfdb3b3799ef1ccff60a81177f78e6b82",
+         x86_64_linux: "74a1e53bc50d6ef6ce1bc67bd9f7b48e549505e0a2453ad4d5ccbc72b0bea874"
+
+  on_macos do
+    version "0.13.2"
+
+    depends_on arch: :arm64
+  end
+  on_linux do
+    version "0.13.2"
+
+    depends_on arch: [:arm64, :x86_64]
+  end
+
+  url "https://github.com/pantsbuild/scie-pants/releases/download/v#{version}/#{artifact}",
+      verified: "github.com/pantsbuild/"
   name "Pants"
   desc "Fast, scalable, user-friendly build system for codebases of all sizes"
   homepage "https://pantsbuild.org/"
 
-  depends_on arch: :arm64
-
-  binary "scie-pants-macos-aarch64", target: "pants"
+  binary artifact, target: "pants"
 
   preflight_steps do
     if_path_exists("pants", base: :binarydir) do
